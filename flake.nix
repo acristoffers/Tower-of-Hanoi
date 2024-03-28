1460fdfcc0a7ddb44ffc585b8eef1817f152f93a
Álan Crístoffer <acristoffers@startmail.com>
2024-03-28 17:33:13 -0300

    chore: lint flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   description = "Tower of Hanoi";
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    flake-utils.url = github:numtide/flake-utils;
+    flake-utils.url = "github:numtide/flake-utils";
 
-    gitignore.url = github:hercules-ci/gitignore.nix;
+    gitignore.url = "github:hercules-ci/gitignore.nix";
     gitignore.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, flake-utils, gitignore }:
@@ -15,14 +15,15 @@
         inherit (gitignore.lib) gitignoreSource;
       in
       rec {
-        packages.tower-of-hanoi = pkgs.flutter.buildFlutterApplication rec {
+        formatter = pkgs.nixpkgs-fmt;
+        packages.tower-of-hanoi = pkgs.flutter.buildFlutterApplication {
           pname = "tower-of-hanoi";
           version = "1.0.0";
           src = gitignoreSource ./.;
           autoPubspecLock = ./pubspec.lock;
         };
         packages.default = packages.tower-of-hanoi;
-        devShell = with pkgs; mkShell rec {
+        devShell = with pkgs; mkShell {
           buildInputs = [ flutter ];
           nativeBuildInputs = [ busybox ];
         };
